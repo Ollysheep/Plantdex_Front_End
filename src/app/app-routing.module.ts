16a8e75dc2feb04046c1ebc95d9a9a936818391b
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2023-12-15 10:36:31 +0100

    3 - routing principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { PageMyPlantsComponent } from './pages/page-my-plants/page-my-plants.component';
+import { PageAdminComponent } from './pages/page-admin/page-admin.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Set route for url : http://localhost:4200
+  { path: '', component: PageHomeComponent },
+  //Set route for url : http://localhost:4200/my-plants
+  { path: 'my-plants', component: PageMyPlantsComponent },
+  //Set route for url : http://localhost:4200/admin -> PageAdminComponent
+  { path: 'admin', component: PageAdminComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
